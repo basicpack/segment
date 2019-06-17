21e81c4d1ace9b524c05362eb0f85bdcc0dce95e
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-06-17 16:08:00 -0300

    M  segment.php

--- segment.php
+++ segment.php
@@ -3,15 +3,26 @@
 //24 de agosto de 2018
 
 function segment($segmentId=null){
+    $arr=parse_url($_ENV['SITE_URL']);
+    if(isset($arr['path']) && $arr['path']<>'/'){
+        $arr=explode('/',$arr['path']);
+        $arr=array_filter($arr);
+        $numeroDePastas=count($arr);
+    }else{
+        $numeroDePastas=0;
+    }
     $str=$_SERVER["REQUEST_URI"];
     $str=@explode('?',$str)[0];
     $arr=explode('/',$str);
     $arr=array_filter($arr);
     $arr=array_values($arr);
+    $i=1;
+    if($numeroDePastas>0){
+        $i=$i-$numeroDePastas;
+    }
     if(count($arr)<1){
-        $segment[1]='/';
+        $segment[$i]='/';
     }else{
-        $i=1;
         foreach ($arr as $key => $value) {
             $segment[$i++]=$value;
         }
@@ -26,3 +37,4 @@ function segment($segmentId=null){
         }
     }
 }
+
